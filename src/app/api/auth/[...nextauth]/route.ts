296368593aa9c69ff98a5ab2f45e494e8cacc6c9
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-08-29 06:55:17 +0800

    moved authOptions to separate folder (imprt issue)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,5 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
 
-const prisma = new PrismaClient();
-
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  adapter: PrismaAdapter(prisma) as Adapter,
-});
-
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
