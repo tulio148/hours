36c1d8500e38d38ba55496b73c13f81caa0bfb02
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-11-15 05:33:19 +0800

    fixed activity component

--- src/ui/activity.tsx
+++ src/ui/activity.tsx
@@ -1,33 +1,47 @@
 "use client";
 import { useActivity } from "@/providers/activityProvider";
+import { useTime } from "@/providers/timerProvider";
 import { hour, minutes, seconds } from "@/lib/utils/timeFormatter";
-import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faCircleH, faClock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ActivityType } from "@/lib/types/activity.types";
 
 export default function Activity({ activity }: { activity: ActivityType }) {
-  const { setActivityName, activitySelected, setActivitySelected } =
-    useActivity();
+  const {
+    setActivityTime,
+    setActivityName,
+    activitySelected,
+    setActivitySelected,
+  } = useActivity();
+  const { time, setTime, isOn, setIsOn, setDisplayIsHidden } = useTime();
 
   const selectActivity = (id: number) => {
     setActivitySelected(id);
   };
 
-  const id = activity.id;
-
-  if (activitySelected === id) {
-    setActivityName(activity.name), setActivitySelected(activity.id);
-  }
+  // if (activitySelected === id) {
+  //   setActivityName(activity.name), setActivitySelected(activity.id);
+  // }
 
   return (
     <div className="w-full">
       {activitySelected != activity.id && (
-        <button onClick={() => selectActivity(id)}>{activity.name}</button>
+        <button
+          onClick={() => {
+            selectActivity(activity.id);
+            setActivityName(activity.name);
+            setActivityTime(0);
+          }}
+        >
+          {activity.name}
+        </button>
       )}
 
       {activitySelected === activity.id && (
         <div className="w-full">
-          <button onClick={() => selectActivity(0)}>{activity.name}</button>
+          <button onClick={() => selectActivity(activity.id)}>
+            {activity.name}
+          </button>
           <div className="float-right flex gap-2 items-center ">
             <FontAwesomeIcon
               icon={faClock}
