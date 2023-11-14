c916170d7c1af19bacd236e747d123e66477ae14
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-11-15 05:32:28 +0800

    fixed activity type

--- src/lib/types/activity.types.ts
+++ src/lib/types/activity.types.ts
@@ -2,7 +2,7 @@ export type ActivityType = {
   id: number;
   name: string;
   hours: number;
-  userId: number | null; // Assuming userId can be null or a number
-  createdAt: string;
-  updatedAt: string;
+  userId: string | null;
+  createdAt: Date;
+  updatedAt: Date;
 };
