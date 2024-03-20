30891de07b75b6b7dd36c2ec3fcb2cbabd8a47b2
Tulio Gerbassi <tuliogerbassi@gmail.com>
2024-03-20 20:50:41 +0800

    updated prisma instantiation code

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-// Docs about instantiating `PrismaClient` with Next.js:
-// https://pris.ly/d/help/next-js-best-practices
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
