f21c0d4b8694349cb3f36b66c85730fbb145b55a
Tulio Gerbassi <tuliogerbassi@gmail.com>
2024-03-23 18:20:28 +0800

    adapted timer, and localstorage logic for the new variables

--- src/ui/timer.tsx
+++ src/ui/timer.tsx
@@ -4,33 +4,81 @@ import { useTime } from "@/providers/timerProvider";
 import { useActivity } from "@/providers/activityProvider";
 import Display from "./display";
 import Controls from "./controls";
+import { useUser } from "@clerk/nextjs";
 
 export default function Timer() {
-  const { time, setTime, isOn } = useTime();
-  const { activityName } = useActivity();
+  const {
+    startTime,
+    time,
+    displayTime,
+    setDisplayTime,
+    setTime,
+    isOn,
+    setIsOn,
+    setStartTime,
+  } = useTime();
+
+  const { activityName, activitySelected, setActivitySelected } = useActivity();
+  const { user } = useUser();
+  const userId = user?.id;
 
   useEffect(() => {
     let interval: NodeJS.Timeout;
     if (isOn) {
-      interval = setInterval(() => setTime(time + 1), 1000);
+      interval = setInterval(() => {
+        setDisplayTime(time + (Date.now() - startTime) / 1000);
+      }, 1000);
     }
     return () => clearInterval(interval);
-  }, [isOn, time, setTime]);
+  }, [isOn, time, displayTime]);
 
   useEffect(() => {
-    const savedTime = localStorage.getItem("time");
-    if (savedTime) {
-      setTime(JSON.parse(savedTime));
+    if (userId) {
+      const userPrefix = `user-${userId}-`;
+      const loadIsOn = localStorage.getItem(userPrefix + "isOn");
+      const loadStartTime = localStorage.getItem(userPrefix + "startTime");
+      const loadTime = localStorage.getItem(userPrefix + "time");
+      const loadActivitySelected = localStorage.getItem(
+        userPrefix + "activitySelected"
+      );
+
+      try {
+        if (loadIsOn) {
+          setIsOn(JSON.parse(loadIsOn));
+        }
+        if (loadStartTime) {
+          setStartTime(JSON.parse(loadStartTime));
+        }
+        if (loadTime) {
+          setTime(JSON.parse(loadTime));
+        }
+        if (loadActivitySelected) {
+          setActivitySelected(JSON.parse(loadActivitySelected));
+        }
+      } catch (error) {
+        console.error("Error loading data from localStorage:", error);
+      }
     }
-  }, [setTime]);
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem("time", JSON.stringify(time));
-  }, [time]);
+    if (userId) {
+      const userPrefix = `user-${userId}-`;
+
+      localStorage.setItem(userPrefix + "isOn", JSON.stringify(isOn));
+
+      localStorage.setItem(userPrefix + "startTime", JSON.stringify(startTime));
+      localStorage.setItem(
+        userPrefix + "activitySelected",
+        JSON.stringify(activitySelected)
+      );
+      localStorage.setItem(userPrefix + "time", JSON.stringify(time));
+    }
+  }, [isOn, startTime, activitySelected, userId]);
 
   return (
     <>
-      {activityName != "" && (
+      {(activityName != "" || activitySelected != 0) && (
         <div className="flex flex-col gap-5 mt-10 min-w-[300px] max-w-5xl">
           <Display />
           <Controls />
